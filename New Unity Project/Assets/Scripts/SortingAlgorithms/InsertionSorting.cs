1122120383c9e2a16ae66959d8ee1a8792ebfadb
Freemort <freemortus@gmail.com>
2020-05-14 12:00:29 +0300

    Insertion sort coloring fix
    -Fixed insertion sort coloring bug.

--- New Unity Project/Assets/Scripts/SortingAlgorithms/InsertionSorting.cs
+++ New Unity Project/Assets/Scripts/SortingAlgorithms/InsertionSorting.cs
@@ -9,14 +9,20 @@ public class InsertionSorting : SortingAlgorithmBase
     {
         for (int i = 1; i < Array.Count; i++)
         {
-            CompareElements(i, i - 1);
             if (Array[i] >= Array[i - 1])
+            {
+                CompareElements(i, i - 1);
+                yield return i;
                 continue;
+            }
 
             for (int a = i; a > 0; a--)
             {
+                //if (a == i && i != 0)
+                //    continue;
                 int b = a - 1;
                 CompareElements(i, b);
+                yield return a;
                 if (Array[i] >= Array[b])
                 {
                     ShiftArray(b + 1, i, Array);
@@ -28,9 +34,8 @@ public class InsertionSorting : SortingAlgorithmBase
                     break;
                 }
 
-                yield return a;
             }
-            yield return i;
+            //yield return i;
         }
 
         FinishSorting();
